752c6eafb82cfc7c9792a8bf9d38a553551c5847
shrutinimai <nimaishruti@gmail.com>
2025-03-07 16:43:03 +0530

    connecting with mysql1 updated

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const db = require('./util/database');
+const sequelize = require('./util/database');
+
 
 const app = express();
 
@@ -21,8 +22,13 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
+sequelize
+.sync()
+.then(result=>{
+  console.log(result);
+  app.listen(3000);
+})
+.catch(err=>{
+  console.log(err);
+})
 
-const PORT = process.env.PORT || 8000; 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
--- util/database.js
+++ util/database.js
@@ -1,10 +1,9 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
+const sequelize = new Sequelize('mydb','root','root',{
+    dialect:'mysql',
     host: 'localhost',
-    user: 'root',
-    database: 'mydb',
-    password: 'root'
+
 });
 
-module.exports = pool.promise();
+module.exports = sequelize
